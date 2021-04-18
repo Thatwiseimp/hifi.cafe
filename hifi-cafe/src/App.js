9269401db568f5c4546f8dd4d66df689a5dfacd5
ramprakash <ramprakashsmail@gmail.com>
2021-04-18 18:42:50 +0530

    ADD:  styles and gif background

--- hifi-cafe/src/App.js
+++ hifi-cafe/src/App.js
@@ -6,12 +6,13 @@ import PauseIcon from '@material-ui/icons/Pause';
 import FastForwardIcon from '@material-ui/icons/FastForward';
 import {useState, useEffect, useRef} from 'react'
 import {storage} from './firebase'
+import head from './image/head.png'
 
 
 
 function App() {
   const [songlist,_setsonglist]= useState([]);
-  const [image,setimage]= useState("http://giphy.com/gifs/MuztdWJQ4PR7i")
+  const [image,setimage]= useState("https://media0.giphy.com/media/YQbWaRk0MoZ5m/giphy.gif?cid=6a29bdc4w4t3fi0uc0r2t3cg587cvd5m42y91y4cxzc1ltoj&rid=giphy.gif&ct=g")
   const [playing,setplaying]= useState(false);
   const [myAudio,setaudio] = useState(new Audio("https://firebasestorage.googleapis.com/v0/b/hifi-cafe.appspot.com/o/oh_no_oh_no.mp3?alt=media&token=375d5278-3b2a-4d1a-95e3-51d8790e75cd"));
   const [recents,setrecents] = useState([])
@@ -55,31 +56,25 @@ function App() {
   }
 
   function setgif(){
-    let data;
-    const giphy = require('giphy-api-without-credentials')('LW4KBeDgzRVgzEe7kZYmFIBZmJtNVus5');
-    giphy.search('pokemon', function(err, res) {
-      console.log(res) 
-      res.data.map((data)=>setimage(data.url))
-});
-
     let url = `https://api.giphy.com/v1/gifs/search?api_key=LW4KBeDgzRVgzEe7kZYmFIBZmJtNVus5&q=depressed`
     
     fetch(url).then(response => response.json())
-    .then(data=> { setimage(data.data[Math.floor(Math.random()*data.data.length)].url)})
-    // .then(data=> { console.log(data.data )})
+    .then(data=> { setimage(data.data[Math.floor(Math.random()*data.data.length)].images.downsized.url)})
+    // .then(data=> { console.log(data )})
     .catch(err => console.error(err))
     // console.log(res);
   }
 
   function getprev(){
-    myAudio.src = songlist[recents.length-2].url;
-    setdetails(songlist[recents.length-2].name);
+    myAudio.src = recents[recents.length-2].url;
+    setdetails(recents[recents.length-2].name);
     let newlist=[...recents]
     newlist.pop()
     console.log(newlist)
     setrecents(newlist)
     myAudio.play();
     setplaying(true)
+    setgif()
   }
 
   function pause(){
@@ -103,6 +98,7 @@ function App() {
     setdetails(mysonglist[rand_int].name);
     myAudio.play();
     setplaying(true)
+    setgif();
     }
 
 
@@ -113,22 +109,27 @@ function App() {
   },[])
 
   return (
-    <Container>
+    <Container className='cont' style={{backgroundImage: `url(${image})`}}>
       <Banner>
-        <h1>Hi-fi.cafe ☕</h1>
-        <Tools>
-          { recents.length > 1 ? <FastRewindIcon onClick={getprev}/> : null}
-          { (playing)  ? <PauseIcon onClick={pause}/> : <PlayArrowIcon onClick={play}/> }
+        <div classname='hd'>
+          <img  id="my_image" src={head}></img>
+
+
+        </div>
+        <Tools className='toolbar'>
+          { recents.length > 1 ? <FastRewindIcon onClick={getprev} style={{ fontSize: 60 , padding: 15, color: 'black'}}/> : null}
+          { (playing)  ? <PauseIcon onClick={pause} style={{ fontSize: 60, padding: 15 , color: 'black'}} /> : <PlayArrowIcon onClick={play} style={{ fontSize: 60, padding: 15, color: 'black' }} /> }
           
-          <FastForwardIcon onClick={rand_play}/>
-          <Info>{details}</Info>
+          <FastForwardIcon onClick={rand_play} style={{ fontSize: 60, padding: 15, color: 'black' }}/>
+         
 
         </Tools>
+        <Info className='info'>{details}</Info>
       </Banner>
       <Content>
         {/* {document.createElement('image',image)} */}
         {/* <div style={{backgroundImage:` url(${image})`}}></div> */}
-        <img src='${image}' crossOrigin="anonymous" />.
+        {/* <img src='${image}' crossOrigin="anonymous" />. */}
 
 
 
@@ -139,9 +140,13 @@ function App() {
 
 export default App;
 const Container = styled.div`
+
   background-color: black;
-  background-image:url('https://tenor.com/7Xyh.gif');
-  min-height: 100vh;
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  width: 100vw;
+  height: 100vh;
   padding: 0px;
   margin: 0px;
 `
@@ -154,9 +159,8 @@ const Content= styled.div`
   text-align: center;
 `
 const Tools= styled.div`
-  height:30px;
-  width: 30px;
 `
 const Info = styled.div `
-  font-family: Courier new;
+
+  
 `
